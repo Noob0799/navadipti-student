ae9d46fb8abf150b9ba019404d305f66eb714f8c
Noob0799 <uchihaitachi0799@gmail.com>
2020-04-15 01:39:09 +0530

    Prep for deploy 1

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const homeworkRoutes = require('./routes/homework');
 const announcementRoutes = require('./routes/announcement');
 const queryRoutes = require('./routes/query');
 const mongoose = require('mongoose');
-
+const path = require("path");
 const app = express();
 const port = process.env.PORT || "5000";
-app.use(bodyParser.json({limit: '10MB', extended: true}))
-app.use(bodyParser.urlencoded({limit: '10MB', extended: true}))
-
+app.use(express.json({limit: '10MB', extended: true}))
+app.use(express.urlencoded({limit: '10MB', extended: true}))
+app.use(express.static(path.join(__dirname, "frontend", "build")))
 mongoose.connect("mongodb+srv://noob:XHfaVoZsq8AvbqC8@cluster0-atyzx.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true,  useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to database!");
@@ -31,6 +31,9 @@ app.use('/syllabus', syllabusRoutes);
 app.use('/homework', homeworkRoutes);
 app.use('/announcement', announcementRoutes);
 app.use('/query', queryRoutes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
